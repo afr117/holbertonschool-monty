9ed016983685506c7f86fe049bada7b42a099bae
afr117 <alfredo.figueroa07@yahoo.com>
2023-08-09 08:15:16 -0700

    1..1000

--- opcode_push.c
+++ opcode_push.c
@@ -1,18 +1,11 @@
 #include "monty.h"
+#include <stdio.h>
 
 void pall(void) {
-    // Implementation of the pall function
-}
+    int i;
 
-void push(int value) {
-    if (stack_size >= STACK_MAX_SIZE) {
-        fprintf(stderr, "Error: Stack overflow\n");
-        exit(EXIT_FAILURE);
+    for (i = stack_size - 1; i >= 0; i--) {
+        printf("%d\n", data_stack[i]);
     }
-
-    data_stack[stack_size] = value;
-    stack_size++;
 }
-
-/* Other opcode functions here */
 
