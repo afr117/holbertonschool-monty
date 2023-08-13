10a994fcc8cf5b4070f754bc349177029d0c9d74
afr117 <alfredo.figueroa07@yahoo.com>
2023-08-12 22:00:19 -0700

    Updated files: monty.c, monty.h, opcode_push.c

--- opcode_push.c
+++ opcode_push.c
@@ -1,17 +1,20 @@
 #include "monty.h"
 
-void push(int value) {
+void push(char *value_str) {
     if (stack_size >= STACK_MAX_SIZE) {
         fprintf(stderr, "Error: Stack overflow\n");
         exit(EXIT_FAILURE);
     }
 
-    data_stack[stack_size] = value;
-    stack_size++;
-}
-
-void pall(void) {
-    for (size_t i = stack_size; i > 0; i--) {
-        printf("%d\n", data_stack[i - 1]);
+    int value;
+    if (isdigit(value_str[0]) || (value_str[0] == '-' && isdigit(value_str[1]))) {
+        value = atoi(value_str);
+        data_stack[stack_size] = value;
+        stack_size++;
+    } else {
+        fprintf(stderr, "L%d: usage: push integer\n", line_number);
+        fclose(file);
+        exit(EXIT_FAILURE);
     }
 }
+
