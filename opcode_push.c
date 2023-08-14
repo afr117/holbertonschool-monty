37e3838a5a9b141acde32a186cafbd48e34f9429
afr117 <alfredo.figueroa07@yahoo.com>
2023-08-14 06:47:13 -0700

    Updated files: monty.c, monty.h, opcode_push.c

--- opcode_push.c
+++ opcode_push.c
@@ -10,14 +10,24 @@ void push(char *value_str, int line_number) {
         exit(EXIT_FAILURE);
     }
 
-    for (size_t i = 0; value_str[i]; i++) {
-        if (!isdigit(value_str[i]) && value_str[i] != '-' && value_str[i] != '+') {
+    // Check for extra spaces before/after the value string
+    char *trimmed_value_str = value_str;
+    while (*trimmed_value_str == ' ')
+        trimmed_value_str++;
+
+    if (*trimmed_value_str == '\0') {
+        fprintf(stderr, "L%d: usage: push integer\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    for (size_t i = 0; trimmed_value_str[i]; i++) {
+        if (!isdigit(trimmed_value_str[i]) && trimmed_value_str[i] != '-' && trimmed_value_str[i] != '+') {
             fprintf(stderr, "L%d: usage: push integer\n", line_number);
             exit(EXIT_FAILURE);
         }
     }
 
-    value = atoi(value_str);
+    value = atoi(trimmed_value_str);
 
     if (stack_size >= STACK_MAX_SIZE) {
         fprintf(stderr, "Error: Stack overflow\n");
@@ -33,4 +43,3 @@ void pall(void) {
         printf("%d\n", data_stack[i - 1]);
     }
 }
-
