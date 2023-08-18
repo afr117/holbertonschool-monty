195d23ce4e410e283b5642ac16f66af537304c85
afr117 <6666@holbertonstudents.com>
2023-08-18 09:36:07 -0700

    Updated files: monty.c, monty.h, opcode_push.c

--- monty.c
+++ monty.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[]) {
             if (fscanf(file, "%s", value_str) == 1) {
                 push(value_str, line_number);
             } else {
-                fprintf(stderr, "L%d: usage: push integer\n", line_number);
+                fprintf(stderr, "L%d: usage: push integer\n", line_number + 1);
                 fclose(file);
                 exit(EXIT_FAILURE);
             }
