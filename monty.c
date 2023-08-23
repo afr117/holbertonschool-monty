33c1688950c528e21291162fe25424b1d90ca505
afr117 <6666@holbertonstudents.com>
2023-08-23 06:45:43 -0700

    Updated files: monty.c, monty.h, opcode_push.c

--- monty.c
+++ monty.c
@@ -13,14 +13,18 @@
 size_t stack_size = 0;
 int data_stack[STACK_MAX_SIZE];
 
-int main(int argc, char *argv[]) {
-	if (argc != 2) {
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 
 	FILE *file = fopen(argv[1], "r");
-	if (!file) {
+
+	if (!file)
+	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
@@ -28,30 +32,39 @@ int main(int argc, char *argv[]) {
 	char line[100];
 	int line_number = 1;
 
-	while (fgets(line, sizeof(line), file)) {
+	while (fgets(line, sizeof(line), file))
+	{
 		char opcode[100];
 		int num_args = sscanf(line, "\t%s", opcode);
-		if (num_args == 1) {
-			if (strcmp(opcode, "push") == 0) {
+
+		if (num_args == 1)
+		{
+			if (strcmp(opcode, "push") == 0)
+			{
 				char value_str[100];
-				if (sscanf(line, "\t%*s %s", value_str) == 1) {
+
+				if (sscanf(line, "\t%*s %s", value_str) == 1)
+				{
 					push(value_str, line_number);
 				} else {
 					fprintf(stderr, "L%d: usage: push integer\n", line_number);
 					fclose(file);
+
 					exit(EXIT_FAILURE);
 				}
-			} else if (strcmp(opcode, "pall") == 0) {
+			} else if (strcmp(opcode, "pall") == 0)
+			{
 				pall();
 			} else {
 				fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
 				fclose(file);
+
 				exit(EXIT_FAILURE);
 			}
 		}
 		line_number++; // Increment line_number after each line
 	}
 	fclose(file);
+
 	return (0);
 }
-
