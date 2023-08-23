f817715afbf204eb85ef08a1c00e238cee6b20cd
afr117 <6666@holbertonstudents.com>
2023-08-23 14:06:54 -0700

    Updated files: monty.c, monty.h, opcode_push.c

--- monty.c
+++ monty.c
@@ -2,6 +2,10 @@
 #include <string.h>
 #include <ctype.h>
 
+
+size_t stack_size = 0;
+int data_stack[STACK_MAX_SIZE];
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "USAGE: monty file\n");
