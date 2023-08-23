c0cf0e1d65908a07ca488074d79ea3c2002ee162
afr117 <6666@holbertonstudents.com>
2023-08-23 13:54:43 -0700

    Updated files: monty.c, monty.h, opcode_push.c

--- monty.c
+++ monty.c
@@ -2,86 +2,50 @@
 #include <string.h>
 #include <ctype.h>
 
-/* Global variables */
-size_t stack_size = 0;
-int data_stack[STACK_MAX_SIZE];
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
 
-/* Function prototypes */
-int main(int argc, char *argv[]);
-void push(char *value_str, int line_number);
-void pall(void);
+    FILE *file = fopen(argv[1], "r");
+    if (!file) {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
 
-/**
- * main - Entry point for the Monty interpreter.
- * @argc: The number of command line arguments.
- * @argv: An array of pointers to the arguments.
- *
- * Return: Always 0 on success.
- */
+    char line[100];
+    int line_number = 1;
 
-int main(int argc, char *argv[])
-{
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
+    while (fgets(line, sizeof(line), file)) {
+        char opcode[100];
+        int num_args = sscanf(line, " %s", opcode);
 
-	FILE *file = fopen(argv[1], "r");
+        if (num_args == 1) {
+            if (strcmp(opcode, "push") == 0) {
+                char value_str[100];
+                if (sscanf(line, " %*s %s", value_str) == 1) {
+                    push(value_str, line_number);
+                } else {
+                    fprintf(stderr, "L%d: usage: push integer\n", line_number);
+                    fclose(file);
+                    exit(EXIT_FAILURE);
+                }
+            } else if (strcmp(opcode, "pint") == 0) {
+                pint(line_number);
+            } else if (strcmp(opcode, "pall") == 0) {
+                pall();
+            } else {
+                fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+                fclose(file);
+                exit(EXIT_FAILURE);
+            }
+        }
 
-	if (!file)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
+        line_number++;
+    }
 
-	char line[100];
-	int line_number = 1;
-
-	while (fgets(line, sizeof(line), file))
-	{
-		char opcode[100];
-		int num_args = sscanf(line, " %s", opcode);
-
-		if (num_args == 1)
-		{
-			if (strcmp(opcode, "push") == 0)
-			{
-				char value_str[100];
-
-				if (sscanf(line, " %*s %s", value_str) == 1)
-				{
-					push(value_str, line_number);
-				} else
-				{
-					fprintf(stderr, "L%d: usage: push integer\n", line_number);
-					fclose(file);
-					exit(EXIT_FAILURE);
-				}
-			} else if (strcmp(opcode, "pall") == 0)
-			{
-				pall();
-			} else
-			{
-				fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-				fclose(file);
-				exit(EXIT_FAILURE);
-			}
-		}
-		line_number++; /* Increment line_number after each line */
-	}
-
-	fclose(file);
-	return (0);
-}
-
-void pint(int line_number) {
-	if (stack_size > 0) {
-	printf("%d\n", data_stack[stack_size - 1]);
-	} else
-	{
-	fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
-	exit(EXIT_FAILURE);
-}
+    fclose(file);
+    return 0;
 }
 
