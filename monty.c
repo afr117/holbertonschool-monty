38a7f3d620223ea4dac7956d2dbb81eb483c261d
afr117 <6666@holbertonstudents.com>
2023-08-24 01:46:05 -0700

    Updated files: monty.c, monty.h, opcode_push.c

--- monty.c
+++ monty.c
@@ -1,65 +1,27 @@
 #include "monty.h"
-#include <string.h>
-#include <ctype.h>
 
-size_t stack_size = 0;
-int data_stack[STACK_MAX_SIZE] = { -1 } ;
+StackNode *stack = NULL;
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
+        fprintf(stderr, "Usage: %s file\n", argv[0]);
+        return EXIT_FAILURE;
     }
 
     FILE *file = fopen(argv[1], "r");
-    if (!file) {
+    if (file == NULL) {
         fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
+        return EXIT_FAILURE;
     }
 
-    char line[100];
-    int line_number = 1;
-
+    char line[1024];
     while (fgets(line, sizeof(line), file)) {
-        char *token = strtok(line, " \t\n"); // Tokenize the line
-
-        while (token != NULL) {
-            char opcode[100];
-            char arg[100];
-            strcpy(opcode, token);
-
-            token = strtok(NULL, " \t\n"); // Get the next token
-            if (token != NULL) {
-                strcpy(arg, token);
-            } else {
-                arg[0] = '\0';
-            }
-
-            if (strcmp(opcode, "push") == 0) {
-                if (strlen(arg) > 0) {
-                    push(arg, line_number);
-                } else {
-                    fprintf(stderr, "L%d: usage: push integer\n", line_number);
-                    fclose(file);
-                    exit(EXIT_FAILURE);
-                }
-            } else if (strcmp(opcode, "pint") == 0) {
-                pint(line_number);
-            } else if (strcmp(opcode, "pall") == 0) {
-                pall();
-            } else {
-                fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-                fclose(file);
-                exit(EXIT_FAILURE);
-            }
-
-            token = strtok(NULL, " \t\n"); // Get the next token
-        }
-
-        line_number++;
+        // Parse line and execute opcode
+        // Example: sscanf(line, "push %d", &value);
+        // Example: if (strcmp(opcode, "push") == 0) push(value);
     }
 
     fclose(file);
-    return 0;
+    return EXIT_SUCCESS;
 }
 
--- opcode_push.c
+++ opcode_push.c
@@ -1,68 +1,23 @@
 #include "monty.h"
-#include <ctype.h>
 
-/**
- * push - Pushes an element onto the stack
- * @value_str: String representation of the value to be pushed
- * @line_number: Line number of the push instruction in the file
- */
-void push(char *value_str, int line_number) {
-    int value;
-
-    if (!value_str) {
-        fprintf(stderr, "L%d: usage: push integer\n", line_number);
+/* Implementation of push opcode */
+void push(int value) {
+    StackNode *new_node = malloc(sizeof(StackNode));
+    if (new_node == NULL) {
+        fprintf(stderr, "Error: malloc failed\n");
         exit(EXIT_FAILURE);
     }
-
-    /* Check for extra spaces before/after the value string */
-    char *trimmed_value_str = value_str;
-
-    while (*trimmed_value_str == '\t')
-        trimmed_value_str++;
-    if (*trimmed_value_str == '\0') {
-        fprintf(stderr, "L%d: usage: push integer\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    for (size_t i = 0; trimmed_value_str[i]; i++) {
-        if (!isdigit(trimmed_value_str[i]) &&
-            trimmed_value_str[i] != '-' &&
-            trimmed_value_str[i] != '+') {
-            fprintf(stderr, "L%d: usage: push integer\n", line_number);
-            exit(EXIT_FAILURE);
-        }
-    }
-    value = atoi(trimmed_value_str);
-
-    if (stack_size >= STACK_MAX_SIZE) {
-        fprintf(stderr, "Error: Stack overflow\n");
-        exit(EXIT_FAILURE);
-    }
-    data_stack[stack_size] = value;
-    stack_size++;
-
-    pint(line_number);
+    new_node->data = value;
+    new_node->next = stack;
+    stack = new_node;
 }
 
-/**
- * pint - Prints the value at the top of the stack
- * @line_number: Line number of the pint instruction in the file
- */
-void pint(int line_number) {
-    if (stack_size > 0) {
-        printf("%d\n", data_stack[stack_size - 1]);
-    } else {
-        fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-      fflush(stdout);
-}
-
-/**
- * pall - Prints all elements in the stack
- */
+/* Implementation of pall opcode */
 void pall(void) {
-    for (size_t i = stack_size; i > 0; i--) {
-        printf("%d\n", data_stack[i - 1]);
+    StackNode *current = stack;
+    while (current != NULL) {
+        printf("%d\n", current->data);
+        current = current->next;
     }
 }
 
