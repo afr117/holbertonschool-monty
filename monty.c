b7da1d2246b1075f78e95f8da39c3bd76278b0a1
afr117 <6666@holbertonstudents.com>
2023-08-24 07:42:25 -0700

    Updated files: monty.c, monty.h, opcode_push.c

--- monty.c
+++ monty.c
@@ -1,90 +1,92 @@
 #include "monty.h"
-#include <string.h>
-#include <ctype.h>
+
+/**
+ * main - Entry point for Monty interpreter.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of command-line argument strings.
+ *
+ * This function reads and interprets Monty bytecode from file.
+ * Parses Monty instructions and executes accordingly.
+ *
+ * Return: 0 on success, EXIT_FAILURE on failure.
+ */
+
+size_t stack_size = 0;
+int data_stack[STACK_MAX_SIZE] = { -1 };
 
 StackNode *stack = NULL;
 
-/* Global variables */
-size_t stack_size = 0;
-int data_stack[STACK_MAX_SIZE];
-
-/* Function prototypes */
-int main(int argc, char *argv[]);
-void push(char *value_str, int line_number);
-void pall(void);
-
-/**
- * main - Entry point for the Monty interpreter.
- * @argc: The number of command line arguments.
- * @argv: An array of pointers to the arguments.
- *
- * Return: Always 0 on success.
- */
-
 int main(int argc, char *argv[])
 {
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
 
-	FILE *file = fopen(argv[1], "r");
+    FILE *file = fopen(argv[1], "r");
 
-	if (!file)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
+    if (!file)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
 
-	char line[100];
-	int line_number = 1;
+    char line[100];
+    int line_number = 1;
 
-	while (fgets(line, sizeof(line), file))
-	{
-		char opcode[100];
-		int num_args = sscanf(line, " %s", opcode);
+    while (fgets(line, sizeof(line), file))
+    {
+        char opcode[100];
+        char arg[100];
+        int num_args = sscanf(line, " %s %s", opcode, arg);
 
-		if (num_args == 1)
-		{
-			if (strcmp(opcode, "push") == 0)
-			{
-				char value_str[100];
+        if (num_args >= 1)
+        {
+            if (strcmp(opcode, "push") == 0)
+            {
+                if (num_args == 2)
+                {
+                    push(arg, line_number);
+                }
+                else
+                {
+                    fprintf(stderr, "L%d: usage: push integer\n", line_number);
+                    fclose(file);
+                    exit(EXIT_FAILURE);
+                }
+            }
+            else if (strcmp(opcode, "pint") == 0)
+            {
+                pint(line_number); // Call the pint function
+            }
+            else if (strcmp(opcode, "pall") == 0)
+            {
+                pall();
+            }
+            else
+            {
+                fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+                fclose(file);
+                exit(EXIT_FAILURE);
+            }
+        }
 
-				if (sscanf(line, " %*s %s", value_str) == 1)
-				{
-					push(value_str, line_number);
-				} else
-				{
-					fprintf(stderr, "L%d: usage: push integer\n", line_number);
-					fclose(file);
-					exit(EXIT_FAILURE);
-				}
-			} else if (strcmp(opcode, "pall") == 0)
-			{
-				pall();
-			} else
-			{
-				fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-				fclose(file);
-				exit(EXIT_FAILURE);
-			}
-		}
-		line_number++; /* Increment line_number after each line */
-	}
+        line_number++;
+    }
 
-	fclose(file);
-	return (0);
+    fclose(file);
+    return 0;
 }
 
-/**
- * pint - Prints the value at the top of the stack.
- * @line_number: Line number of the pint instruction in the file.
- */
-void pint(int line_number) {
-    if (stack_size > 0) {
-        printf("%d\n", stack->data);
-    } else {
+void pint(int line_number)
+{
+    if (stack_size > 0)
+    {
+        printf("%d\n", data_stack[stack_size - 1]);
+    }
+    else
+    {
         fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
         exit(EXIT_FAILURE);
     }
