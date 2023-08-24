52210433a688533d9110d1bc711f6e82676b64f4
afr117 <6666@holbertonstudents.com>
2023-08-24 03:09:43 -0700

    Updated files: monty.c, monty.h, opcode_push.c

--- monty.c
+++ monty.c
@@ -1,78 +1,54 @@
 #include "monty.h"
 
-/**
- * main - Entry point for Monty interpreter.
- * @argc: Number of command-line arguments.
- * @argv: Array of command-line argument strings.
- *
- * This function reads and interprets Monty bytecode from file.
- * Parses Monty instructions and executes accordingly.
- *
- * Return: 0 on success, EXIT_FAILURE on failure.
- */
-
 size_t stack_size = 0;
 int data_stack[STACK_MAX_SIZE] = { -1 };
-
 StackNode *stack = NULL;
 
-int main(int argc, char *argv[])
-{
-	stack = NULL; // Initialize the stack to NULL
+void execute_instruction(char *opcode, char *arg, int line_number) {
+    if (strcmp(opcode, "push") == 0) {
+        if (*arg != '\0') {
+            push(arg, line_number);
+        } else {
+            fprintf(stderr, "L%d: usage: push integer\n", line_number);
+            exit(EXIT_FAILURE);
+        }
+    } else if (strcmp(opcode, "pall") == 0) {
+        pall();
+    } else {
+        fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+        fclose(file);
+        exit(EXIT_FAILURE);
+    }
+}
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
 
-	FILE *file = fopen(argv[1], "r");
+    FILE *file = fopen(argv[1], "r");
+    if (!file) {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
 
-	if (!file)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
+    char line[100];
+    int line_number = 1;
 
-	char line[100];
-	int line_number = 1;
+    while (fgets(line, sizeof(line), file)) {
+        char opcode[100];
+        char arg[100];
+        int num_args = sscanf(line, " %s %s", opcode, arg);
 
-	while (fgets(line, sizeof(line), file))
-	{
-		char opcode[100];
-		char arg[100];
-		int num_args = sscanf(line, " %s %s", opcode, arg);
+        if (num_args >= 1) {
+            execute_instruction(opcode, arg, line_number);
+        }
 
-		if (num_args >= 1)
-		{
-			if (strcmp(opcode, "push") == 0)
-			{
-				if (num_args == 2)
-				{
-					push(arg, line_number);
-				}
-				else
-				{
-					fprintf(stderr, "L%d: usage: push integer\n", line_number);
-					fclose(file);
-					exit(EXIT_FAILURE);
-				}
-			}
-			else if (strcmp(opcode, "pall") == 0)
-			{
-				pall();
-			}
-			else
-			{
-				fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-				fclose(file);
-				exit(EXIT_FAILURE);
-			}
-		}
-		line_number++;
-	}
+        line_number++;
+    }
 
-	fclose(file);
-	return 0;
+    fclose(file);
+    return 0;
 }
 
