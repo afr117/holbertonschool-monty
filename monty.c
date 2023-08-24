9971cf7ab02b1653f3ef97ca3000d7cb30ff8e3b
afr117 <6666@holbertonstudents.com>
2023-08-24 02:47:14 -0700

    Updated files: monty.c, monty.h, opcode_push.c

--- monty.c
+++ monty.c
@@ -16,47 +16,47 @@ int data_stack[STACK_MAX_SIZE] = { -1 };
 StackNode *stack = NULL; // You need to initialize stack to NULL
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
+	if (argc != 2) {
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
 
-    FILE *file = fopen(argv[1], "r");
-    if (!file) {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
+	FILE *file = fopen(argv[1], "r");
+	if (!file) {
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 
-    char line[100];
-    int line_number = 1;
+	char line[100];
+	int line_number = 1;
 
-    while (fgets(line, sizeof(line), file)) {
-        char opcode[100];
-        char arg[100];
-        int num_args = sscanf(line, " %s %s", opcode, arg);
+	while (fgets(line, sizeof(line), file)) {
+		char opcode[100];
+		char arg[100];
+		int num_args = sscanf(line, " %s %s", opcode, arg);
 
-        if (num_args >= 1) {
-            if (strcmp(opcode, "push") == 0) {
-                if (num_args == 2) {
-                    push(arg, line_number);
-                } else {
-                    fprintf(stderr, "L%d: usage: push integer\n", line_number);
-                    fclose(file);
-                    exit(EXIT_FAILURE);
-                }
-            } else if (strcmp(opcode, "pall") == 0) {
-                pall();
-            } else {
-                fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-                fclose(file);
-                exit(EXIT_FAILURE);
-            }
-        }
+		if (num_args >= 1) {
+			if (strcmp(opcode, "push") == 0) {
+				if (num_args == 2) {
+					push(arg, line_number);
+				} else {
+					fprintf(stderr, "L%d: usage: push integer\n", line_number);
+					fclose(file);
+					exit(EXIT_FAILURE);
+				}
+			} else if (strcmp(opcode, "pall") == 0) {
+				pall();
+			} else {
+				fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+				fclose(file);
+				exit(EXIT_FAILURE);
+			}
+		}
 
-        line_number++;
-    }
+		line_number++;
+	}
 
-    fclose(file);
-    return 0;
+	fclose(file);
+	return 0;
 }
 
