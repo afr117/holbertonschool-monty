917970d6f099421c996d4916268285d87cd42240
afr117 <6666@holbertonstudents.com>
2023-08-24 03:34:25 -0700

    Updated files: monty.c, monty.h, opcode_push.c

--- opcode_push.c
+++ opcode_push.c
@@ -5,16 +5,20 @@
  * @value_str: String representation of the value to be pushed
  * @line_number: Line number of the push instruction in the file
  */
-void push(char *value_str, int line_number) {
+void push(char *value_str, int line_number)
+{
 	int value;
 
-	if (!value_str) {
+	if (!value_str)
+{
 	fprintf(stderr, "L%d: usage: push integer\n", line_number);
 	exit(EXIT_FAILURE);
 }
 
-	for (size_t i = 0; value_str[i]; i++) {
-	if (!isdigit(value_str[i]) && value_str[i] != '-' && value_str[i] != '+') {
+	for (size_t i = 0; value_str[i]; i++)
+{
+	if (!isdigit(value_str[i]) && value_str[i] != '-' && value_str[i] != '+')
+{
 	fprintf(stderr, "L%d: usage: push integer\n", line_number);
 	exit(EXIT_FAILURE);
 }
@@ -22,19 +26,24 @@ void push(char *value_str, int line_number) {
 
 	value = atoi(value_str);
 
-	if (stack_size >= STACK_MAX_SIZE) {
+	if (stack_size >= STACK_MAX_SIZE)
+{
 	fprintf(stderr, "Error: Stack overflow\n");
 	exit(EXIT_FAILURE);
 }
 	data_stack[stack_size] = value;
+
 	stack_size++;
 }
 
 /**
  * pall - Prints all elements in the stack
  */
-void pall(void) {
-	for (size_t i = stack_size; i > 0; i--) {
+
+void pall(void)
+{
+	for (size_t i = stack_size; i > 0; i--)
+{
 	printf("%d\n", data_stack[i - 1]);
 }
 }
