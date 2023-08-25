46e13393c7171698c5bd0ee856e1109f8f128f4b
afr117 <6666@holbertonstudents.com>
2023-08-24 21:40:20 -0700

    3

---
+++ init_process.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+StackNode *stack = NULL; // Initialize the stack pointer
+
+void init_stack(void) {
+    // You can set `stack` to NULL here to start with an empty stack
+    stack = NULL;
+}
+
+void process_line(char *line) {
+    // Your implementation of processing a line here
+    // This function should handle parsing the line and executing the appropriate opcodes
+}
+
