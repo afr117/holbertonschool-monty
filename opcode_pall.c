177aaddce609934c8ac429323236bdbe601010d7
afr117 <6666@holbertonstudents.com>
2023-08-24 19:58:08 -0700

    new update

---
+++ opcode_pall.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+
+void pall(void) {
+    StackNode *current = stack;
+
+    while (current != NULL) {
+        printf("%d\n", current->data);
+        current = current->next;
+    }
+}
+
---
+++ opcode_pop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+void pop(int line_number) {
+    if (stack == NULL) {
+        fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    StackNode *temp = stack;
+    stack = stack->next;
+    free(temp);
+    stack_size--;
+}
+
