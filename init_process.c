38303c26d2cc8c8b27d1a709ac42caaa9d36b5d4
afr117 <alfredo.figueroa07@yahoo.com>
2023-12-10 23:34:02 +0000

    test

--- init_process.c
+++ init_process.c
@@ -1,6 +1,10 @@
 #include "monty.h"
 
-StackNode *stack = NULL; // Initialize the stack pointer
+StackNode *stack = NULL; // Initialize the stack pointer//
+
+void init_stack(void) {
+    stack = NULL;
+}
 
 void process_line(char *line) {
     (void)line;
