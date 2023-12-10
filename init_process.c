f0a84c13aa5cd92f49888c5fa924290d432cbafd
afr117 <alfredo.figueroa07@yahoo.com>
2023-12-10 23:29:07 +0000

    task 0 edited init remove void init_stack(void) {     stack = NULL; }

--- init_process.c
+++ init_process.c
@@ -2,11 +2,6 @@
 
 StackNode *stack = NULL; // Initialize the stack pointer
 
-void init_stack(void) {
-    // You can set `stack` to NULL here to start with an empty stack
-    stack = NULL;
-}
-
 void process_line(char *line) {
     (void)line;
 }
