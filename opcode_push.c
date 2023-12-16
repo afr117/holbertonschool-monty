d0b15dcc35b6467a828fb8b674a3fc47253cd03e
afr117 <alfredo.figueroa07@yahoo.com>
2023-12-16 13:58:18 -0800

    file compilation error better output

--- opcode_push.c
+++ opcode_push.c
@@ -1,4 +1,7 @@
 #include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * push - Pushes an element to the stack.
@@ -7,8 +10,16 @@
  */
 void push(stack_t **stack, unsigned int line_number)
 {
-	(void)line_number;
-    int value = 0; // Get the value to push from your bytecode
+    char *value_str = strtok(NULL, TOKEN_DELIMITERS);
+    int value;
+
+    if (!value_str || !is_numeric(value_str))
+    {
+        fprintf(stderr, "L%d: usage: push integer\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    value = atoi(value_str);
 
     stack_t *new_node = malloc(sizeof(stack_t));
     if (!new_node)
@@ -26,3 +37,4 @@ void push(stack_t **stack, unsigned int line_number)
 
     *stack = new_node;
 }
+
