544f8df622229ddd7ace2a5a1d6e432f0ca2de97
afr117 <alfredo.figueroa07@yahoo.com>
2023-12-17 01:51:04 -0800

    tst

--- monty.c
+++ monty.c
@@ -18,8 +18,8 @@ void init_stack(void)
  */
 void process_line(char *line)
 {
-    char *opcode;
-    unsigned int line_number = 0;
+    char *opcode, *value_str;
+    unsigned int line_number = 0; // Initialize line_number
 
     opcode = strtok(line, TOKEN_DELIMITERS);
     if (!opcode || opcode[0] == '#')
@@ -27,12 +27,59 @@ void process_line(char *line)
 
     if (strcmp(opcode, "push") == 0)
     {
-        push(&stack, line_number);
+        value_str = strtok(NULL, TOKEN_DELIMITERS);
+        if (!value_str || !is_numeric(value_str))
+        {
+            fprintf(stderr, "L%d: usage: push integer\n", line_number);
+            exit(EXIT_FAILURE);
+        }
+
+        push(&stack, atoi(value_str));
     }
     else if (strcmp(opcode, "pall") == 0)
     {
         pall(&stack, line_number);
     }
     // Add more opcode handling as needed
+}
+
+/**
+ * main - Entry point of the Monty interpreter.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of command-line argument strings.
+ * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure.
+ */
+int main(int argc, char *argv[])
+{
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    FILE *file;
+    unsigned int line_number = 0; // Initialize line_number
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: monty file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    file = fopen(argv[1], "r");
+    if (!file)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    init_stack();
+
+    while ((read = getline(&line, &len, file)) != -1)
+    {
+        line_number++;
+        process_line(line);
+    }
+
+    fclose(file);
+    free(line);
+    exit(EXIT_SUCCESS);
 }
 
